5137b67a508f2eb797b83146c592ee27160d2ed5
Alisa <alisafavorskaa81@gmail.com>
2025-06-05 13:05:44 +0900

    true

--- diplomka/settings.py
+++ diplomka/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qbt_i7q*6l*n&gh!-_2v&druc9hzpnm)@k5h)-z%()qcb%&p)-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["*"]
 AUTH_USER_MODEL = 'www.User'
 
